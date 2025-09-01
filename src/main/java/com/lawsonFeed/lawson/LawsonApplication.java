e9ee8520a6ce8b01923bf1f26a6d6149108bdd01
EthanWan <ethandi852@gmail.com>
2025-09-01 18:59:14 +0800

    modified LawsonApplication.java with logic of XWPFDocument class to create word document; Also, modified gitignore files to exclude *.docx in git

--- src/main/java/com/lawsonFeed/lawson/LawsonApplication.java
+++ src/main/java/com/lawsonFeed/lawson/LawsonApplication.java
@@ -1,13 +1,35 @@
 package com.lawsonFeed.lawson;
 
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 @SpringBootApplication
 public class LawsonApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LawsonApplication.class, args);
+		System.out.println("Hello World");
+		try(XWPFDocument document = new XWPFDocument()){
+
+			XWPFParagraph paragraph = document.createParagraph();
+			XWPFRun run = paragraph.createRun();
+			run.setText("Hello, this is a word document");
+
+			// save the documents
+			FileOutputStream output = new FileOutputStream("word_output.docx");
+			document.write(output);
+			System.out.println("Document created successfully!");
+
+		} catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 
 }
